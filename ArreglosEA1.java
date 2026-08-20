e0f5e772bab9b128e8b8be1ea0d3a062ca90bd78
evelyngomezr <evelyn.gomezr@est.iudigital.edu.co>
2026-08-20 17:43:33 -0500

    Manipulacion de vectores

---
+++ ArreglosEA1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArreglosEA1 {
+    public class ArreglosEA1 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        // =====================================================
+        // 1. CREAR Y LLENAR EL VECTOR
+        // =====================================================
+        int[] vector = new int[16];
+
+        System.out.println("Manipulación de Vectores en Java");
+        System.out.println("======================================");
+        System.out.println("======================================");
+        
+
+        System.out.println("Ingrese 15 numeros enteros entre 10 y 100:");
+
+
+}
